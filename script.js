aa379eb5a15fb4d6cbe273ecdefd74fc6cae2c2c
Polina Limonova <polina98_08@mail.ru>
2023-04-07 12:10:09 +0300

    final version

--- script.js
+++ script.js
@@ -1,38 +1,4 @@
-const tasks = [
-  // {
-  //   _id: "5d2ca9e2e03d40b326596aa7",
-  //   completed: true,
-  //   body: "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
-  //   title: "1 - Eu ea incididunt sunt consectetur fugiat non.",
-  // },
-  // {
-  //   _id: "5d2ca9e29c8a94095c1288e0",
-  //   completed: false,
-  //   body: "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
-  //   title:
-  //     "2 - Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.",
-  // },
-  // {
-  //   _id: "5d2ca9e2e03d40b3232496aa7",
-  //   completed: true,
-  //   body: "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
-  //   title: "3 - Eu ea incididunt sunt consectetur fugiat non.",
-  // },
-  // {
-  //   _id: "5d2ca9e29c8a94095564788e0",
-  //   completed: false,
-  //   body: "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
-  //   title:
-  //     "4 - Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.",
-  // },
-];
-
 (function () {
-  // Data
-  // const objOfTasks = arrOfTasks.reduce((acc, task) => {
-  //   acc[task._id] = task;
-  //   return acc;
-  // }, {});
   const objOfTasks = JSON.parse(localStorage.getItem("data")) || {};
   const themes = {
     default: {
@@ -206,10 +172,6 @@ const tasks = [
     });
     listContainer.append(fragment1);
     listContainer.append(fragment2);
-
-    // tasksMenuContainer
-    //   .querySelector('[data-type="all"]')
-    //   .classList.add("btn-active");
   }
 
   function renderOneLi({ _id, title, body, completed }) {
